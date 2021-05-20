0d9d7e392f83d4452d9180d77303e536add40054
Miriam-Rachel <m63803@gmail.com>
2021-05-20 23:18:28 +0300

    User authentication
    Add user authentication using Firebase OTP authentincation
    
    Signed-off-by: Miriam-Rachel <m63803@gmail.com>

--- app/src/main/java/com/example/myapplication/OTPActivity.java
+++ app/src/main/java/com/example/myapplication/OTPActivity.java
@@ -1,18 +1,104 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.myapplication.databinding.ActivityOTPBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class OTPActivity extends AppCompatActivity {
+    ActivityOTPBinding binding;
+    FirebaseAuth auth;
 
+    String verificationId;
+
+    ProgressBar dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_o_t_p);
+        binding = ActivityOTPBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null)
             actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.my_gradient));
+
+        //set a progress bar and disable interaction until the code is sent
+        dialog = new ProgressBar(this);
+        dialog.setVisibility(View.VISIBLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+
+        auth = FirebaseAuth.getInstance();
+
+        //get the input phone number
+        String phoneNumber = getIntent().getStringExtra("phoneNumber");
+
+        binding.phoneLbl.setText("Verify " + phoneNumber);
+
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(OTPActivity.this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verifyId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(verifyId, forceResendingToken);
+                        //dismiss the progress bar and get back user interaction
+                        dialog.setVisibility(View.GONE);
+                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                        verificationId = verifyId;
+                    }
+                }).build();
+
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+        binding.otpView.setOtpCompletetionListener(new onOtpCompletionListener() {
+            @Override
+            public void onOtpCompleted(String otp){
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otp);
+
+                auth.signInWithCredential(credential).addOnCanceledListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(OTPActivity.this, "Logged In", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(OTPActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/PhoneNumberActivity.java
+++ app/src/main/java/com/example/myapplication/PhoneNumberActivity.java
@@ -2,17 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.databinding.ActivityPhoneNumberBinding;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PhoneNumberActivity extends AppCompatActivity {
 
+    ActivityPhoneNumberBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null)
             actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.my_gradient));
+
+        binding.phoneBox.requestFocus();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
+                intent.putExtra("phoneNumber", binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
